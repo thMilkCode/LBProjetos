8ec6fab3ae4f42e8a7e14941faf2b83620a9fd08
thmilkcode <th.tarefas@gmail.com>
2020-04-08 16:17:37 -0300

    Projeto1

--- LB1.js
+++ LB1.js
@@ -16,19 +16,21 @@ const name1 = "Thiago"
 const name2 = "Sarah"
 const sexo1 = "M"
 const sexo2 = "F"
-const idade = 55
+const idade = 32
 const contriM = 35
 const contriF = 30
 const somaM = idade + contriM
 const somaF = idade + contriF
+const homemaposenta = sexo1 == "M" && contriM >= 35 && somaM >= 95
+const mulheraposenta = sexo2 == "F" && contriF >= 30 && somaF >= 85
 
-if (somaM >= 95 && sexo1 == "M")
-console.log (`${name1}, você pode se aposentar!`)
-else (somaM < 94.9 && sexo1 == "M")
-console.log (`${name1}, você ainda não pode se aposentar!`)
+if (homemaposenta)
+console.log (`${name1}, você pode aposentar!`)
+else
+console.log (`${name1}, você não pode aposentar!`)
 
-if (somaF >= 85 && sexo2 == "F")
-console.log (`${name2}, você pode se aposentar!`)
-else (somaF < 84.9 && sexo2 == "F")
-console.log (`${name2}, você ainda não pode se aposentar!`)
+if (mulheraposenta)
+console.log (`${name2}, você pode aposentar!`)
+else
+console.log (`${name2}, você não pode aposentar!`)
 
