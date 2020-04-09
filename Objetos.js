d2a7845f0ce20071188e980b45a964f5592441a6
thmilkcode <th.tarefas@gmail.com>
2020-04-08 23:02:15 -0300

    Explicação Objetos e Arrays

---
+++ Objetos.js
@@ -0,0 +1,49 @@
+// Para criar objetos, coloca-se dentro de uma const as chaves {}
+// Os objetos são uma coleção de propriedades e para acessá-las é preciso digitar a variável seguido de . (ponto).
+// No caso abaixo temos objetos de alunos (nome e nota)
+// Para agrupar objetos é só colocar colchetes [] e o nome disso é Array
+
+const aluno01 =     {
+    nome: "Thiago",
+    nota: 6
+}
+
+const aluno02 = {
+    nome: "Sarah",
+    nota: 8
+}
+
+const aluno03 = {
+    nome:"Valentina",
+    nota: 9.5
+}
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+console.log (media)
+
+//  Agrupando valores (Arrays)
+
+const alunos = [
+
+    {
+        name: "Thiago",
+        nota: 6
+    },
+    {
+        name: "Sarah",
+        nota: 8
+    },
+    {
+        name: "Valentina",
+        nota: 9.5
+    }
+]
+
+const nomeDeAlunos = ["Thiago", "Sarah", "Valentina"]
+
+console.log (nomeDeAlunos)
+
+const media1 = alunos[0].nota + alunos[1].nota + alunos[1].nota / 3
+
+console.log (media1)
